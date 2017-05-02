4339d65f5d16bf132c97fc0a9f8747736844cd7c
Ryan Brownell <ryan.brownell@gmail.com>
2017-05-02 19:04:06 -0300

    Completed option field population and inclusion in field settings. Changed Session Views Field to Session Views Flag Field

---
+++ src/Plugin/views/field/SessionViewsFlagField.php
@@ -0,0 +1,188 @@
+<?php
+
+namespace Drupal\session_views\Plugin\views\field;
+
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Component\Utility\Random;
+use Drupal\views\Plugin\views\field\FieldPluginBase;
+use Drupal\views\ResultRow;
+
+/**
+ * A handler to provide a field that is completely custom by the administrator.
+ *
+ * @ingroup views_field_handlers
+ *
+ * @ViewsField("session_views_flag_field")
+ */
+class SessionViewsFlagField extends FieldPluginBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function usesGroupBy() {
+    return FALSE;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function query() {
+    // Do nothing -- to override the parent query.
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function defineOptions() {
+    $options = parent::defineOptions();
+
+    $options['hide_alter_empty'] = ['default' => FALSE];
+    $options['session_views_field'] = array('default' => '');
+    return $options;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildOptionsForm(&$form, FormStateInterface $form_state) {
+    
+    //$entityManager = \Drupal::entityManager();
+    //kint($entityManager->getFieldDefinitions('node', 'position'));
+        
+    //TODO: REVISE UPDATE found at https://drupal.stackexchange.com/questions/167001/field-info-field-deprecated-whats-the-equivalent
+    //kint(\Drupal::entityTypeManager()->getDefinition('node')->getBundleLabel());
+    //kint(\Drupal::entityManager()->getFieldMap()['node']);
+    //kint(\Drupal::entityManager()->getAllBundleInfo()['node']);
+    //kint(\Drupal\field\Entity\FieldConfig::loadByName('node', 'position', 'field_experience_type'));
+    //kint(\Drupal\field\Entity\FieldConfig::loadByName('node', 'position', 'field_experience_type')->getSetting('handler'));
+    //kint(\Drupal\field\Entity\FieldConfig::loadByName('node', 'position', 'field_experience_type')->getSetting('handler_settings'));
+    //kint(\Drupal\field\Entity\FieldConfig::loadByName('node', 'position', 'field_experience_type')->getSetting('handler_settings')['target_bundles']);
+    
+    //TODO: REVISE UPDATE found at https://drupal.stackexchange.com/questions/167001/field-info-field-deprecated-whats-the-equivalent
+        
+    
+    //kint($field_map);
+    
+    //$node_fields = array_keys($node_field_map['node']);
+    
+    $list = $this->getEligibleFields();
+    
+    $form['session_views_field'] = array(
+      '#title' => $this->t('Flag trigger field'),
+      '#type' => 'select',
+      '#options' => $list,
+      '#description' => 'Select the field that will be used to trigger the flag.',
+    );
+    
+    parent::buildOptionsForm($form, $form_state);
+    
+    //kint($this);
+    //kint($form);
+    //kint($form_state);
+    
+//  $field_map = \Drupal::entityManager()->getFieldMap();
+//  $node_field_map = $field_map['node'];
+//  $node_fields = array_keys($node_field_map['node']);
+//  
+//  kint("Hello World");
+  
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function render(ResultRow $values) {
+    
+    kint($this->options);
+    
+    kint($values->nid);
+
+    //return $values->nid;
+  }
+    
+  //TODO: Consider moving the following out into the module class.
+  
+  /**
+   * Gets an array of fields configured to work with Session Views.
+   */
+  private function getEligibleFields() {
+    $ENTITY_TYPE = 'node';
+    $fields = $this->getEntityReferenceFields($ENTITY_TYPE);
+
+    $list = array();
+    
+    foreach (array_keys($fields) as $field)
+    { 
+      $label = "";
+      $first = true;
+      
+      foreach($fields[$field] as $bundle)
+      {
+        if (\Drupal\field\Entity\FieldConfig::loadByName($ENTITY_TYPE, $bundle, $field)->getSetting('handler') == 'default:taxonomy_term')
+        {       
+          if(in_array($this->getTargetVocabulary(), \Drupal\field\Entity\FieldConfig::loadByName($ENTITY_TYPE, $bundle, $field)->getSetting('handler_settings')['target_bundles']))
+          {        
+            if ($first)
+            {
+              $label = \Drupal\field\Entity\FieldConfig::loadByName($ENTITY_TYPE, $bundle, $field)->getLabel();
+              $label .= ' - Appears in: ';
+              $label .= $bundle;
+              $first = false;
+            }
+            else
+            {
+              $label .= ', ';
+              $label .= $bundle;
+            }
+          }
+        }
+      }
+      
+      if (!empty($label))
+      {
+        $list[$field] = $label;
+      }
+    }
+    
+    return $list;
+  }
+  
+  /**
+  * Gets an array of all Entity Reference fields for the specified entity type.
+  * @param string $type A string with the machine name of the entity type.
+  * @return array An array of user created Entity Reference fields.
+  */
+  private function getEntityReferenceFields($type) {
+    $node_field_map = \Drupal::entityManager()->getFieldMap()[$type];
+    
+    $fields = array();
+    
+    foreach (array_keys($node_field_map) as $field_key)
+    {
+      if (strpos($field_key, 'field_') !== false)
+      {
+        if ($node_field_map[$field_key]['type'] == 'entity_reference')
+        {
+          $fields[$field_key] = $node_field_map[$field_key]['bundles'];
+        }
+      }
+    }
+    
+    return $fields;
+  }
+  
+  /**
+   * Gets the key of the currently selected vocabulary in the module's settings.
+   * @return string A string containing the key of the selected vocabulary.
+   */
+  private function getTargetVocabulary() {
+    $config = \Drupal::config('session_views.settings');
+
+    if ($config->get('session_views.selected_vocabulary'))
+    {
+      return $config->get('session_views.selected_vocabulary');
+    }
+    
+    return null;
+  }
+}
